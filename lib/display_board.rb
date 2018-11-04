8a512d7dde4e2fd96d4bd7f3ed1566edc353a68e
karen ercoli <karenercoli@gmail.com>
2018-11-04 13:13:46 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 def display_board
-  print
-     |   |
+  print  
+     |   |   
   -----------
-     |   |
+     |   |   
   -----------
-     |   |
-end# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+     |   |   
+end
+display_board# Define a method display_board that prints a 3x3 Tic Tac Toe Board
